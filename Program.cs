30a62d6c2f7c90a1b1407b05c30175cd277950a0
Sh1nosh1 <shindanotamashi@gmail.com>
2023-12-15 22:07:36 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace горпгор
+{
+    internal class Program
+    {
+        public static double Func(double n, double count)
+        {
+            return count / n;
+        }
+        static void Main(string[] args)
+        {
+            Write("Enter the quantity of numbers you want to enter: ");
+            double n = Convert.ToDouble(ReadLine());
+            double[] numbers = new double[(int)n];
+            double count = 0;
+            WriteLine();
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                Write($"Enter {i + 1} number: ");
+                numbers[i] = Convert.ToDouble(ReadLine());
+                count += numbers[i];
+            }
+
+            double arithmetic = Func(n, count);
+
+            Write($"\nThe arithmetic mean: {arithmetic}");
+            ReadKey();
+
+        }
+    }
+}
